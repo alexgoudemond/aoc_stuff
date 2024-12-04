995272e7503711add043202d73dcfbeec3fe4efe
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2024-12-04 08:05:40 +0200

    Setup Dec2024 Day_04 Puzzle1

--- dec_2024/src/aoc/dec_2024/helper/PuzzleContents.java
+++ dec_2024/src/aoc/dec_2024/helper/PuzzleContents.java
@@ -11,11 +11,26 @@ public class PuzzleContents {
 
     private final List<String[]> puzzleRows;
 
+    private final String[][] puzzleGrid;
+
     public PuzzleContents(List<String> rawPuzzleContent) {
         this.rawPuzzleContent = rawPuzzleContent;
-        this.puzzleRows = rawPuzzleContent.stream()
+        this.puzzleRows = getPuzzleAsRows(rawPuzzleContent);
+        this.puzzleGrid = getContentsAsGrid();
+    }
+
+    private List<String[]> getPuzzleAsRows(List<String> rawPuzzleContent) {
+        return rawPuzzleContent.stream()
                 .map(str -> str.split(" "))
                 .collect(Collectors.toList());
+    }
+
+    private String[][] getContentsAsGrid() {
+        String[][] grid = new String[puzzleRows.size()][puzzleRows.get(0).length];
+        for (int i = 0; i < puzzleRows.size(); i++) {
+            grid[i] = puzzleRows.get(i);
+        }
+        return grid;
     }
 
     public List<String> getRawPuzzleContent() {
@@ -34,6 +49,10 @@ public class PuzzleContents {
         return puzzleRows.size();
     }
 
+    public String[][] getPuzzleGrid() {
+        return puzzleGrid;
+    }
+
     public void forEach(Consumer<String> consumer) {
         rawPuzzleContent.forEach(consumer);
     }
@@ -50,5 +69,9 @@ public class PuzzleContents {
         return puzzleRows.stream()
                 .map(Arrays::toString)
                 .collect(Collectors.toList());
+    }
+
+    public void showAsGrid() {
+        rawPuzzleContent.forEach(row -> System.out.println(row.toCharArray()));
     }
 }
