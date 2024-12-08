5abba7012e9e4186de0d94b36f06cb8807245168
Alex Blake-Goudemond <alexgoudemond@gmail.com>
2024-12-08 07:01:08 +0200

    Still failing on dec_2024 day_07 puzzle 1

--- dec_2024/src/aoc/dec_2024/helper/MathHelper.java
+++ dec_2024/src/aoc/dec_2024/helper/MathHelper.java
@@ -1,12 +1,31 @@
 package aoc.dec_2024.helper;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MathHelper {
 
     private MathHelper() {}
 
-    public static int evalLeftToRight(String expression){ // "81+40*27+19*4"
-        // str.split("[\\*\\+]") isolates numbers
-        // str.split("(\\d+)") to isolate numbers
-        return -1;
+    public static long evalLeftToRight(String expression){
+        List<Long> inputs = Arrays.stream(expression.split("[\\+\\*]"))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+        List<String> operators = Arrays.stream(expression.split("(\\d+)"))
+                .filter(str -> !str.isEmpty())
+                .collect(Collectors.toList());
+        long startingInput = inputs.get(0);
+        int nextIndex = 1;
+        long result = startingInput;
+        for (String operator : operators) {
+            long nextInput = inputs.get(nextIndex++);
+            if (operator.equals("+")) {
+                result = result + nextInput;
+            }else{
+                result = result * nextInput;
+            }
+        }
+        return result;
     }
 }
