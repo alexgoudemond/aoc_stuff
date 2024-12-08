0a53172c5cfe117eb4d63012103925c9599743eb
Alex Goudemond <alex@psybergate.co.za>
2024-12-08 07:01:08 +0200

    Still failing on dec_2024 day_07 puzzle 1

--- dec_2024/src/aoc/dec_2024/day_07/BridgeRepair.java
+++ dec_2024/src/aoc/dec_2024/day_07/BridgeRepair.java
@@ -1,5 +1,6 @@
 package aoc.dec_2024.day_07;
 
+import aoc.dec_2024.helper.MathHelper;
 import aoc.dec_2024.helper.PuzzleContents;
 import aoc.dec_2024.helper.PuzzleInputLoader;
 import aoc.dec_2024.helper.PuzzleInputLoaderImpl;
@@ -15,33 +16,45 @@ public class BridgeRepair {
 
     public BridgeRepair() {
         PuzzleInputLoader puzzleInputLoader = new PuzzleInputLoaderImpl("");
-        PuzzleContents puzzleContents = puzzleInputLoader.getPuzzleContents("day_07/BridgeRepairTest.txt");
+//        PuzzleContents puzzleContents = puzzleInputLoader.getPuzzleContents("day_07/BridgeRepairTest.txt");
+        PuzzleContents puzzleContents = puzzleInputLoader.getPuzzleContents("day_07/BridgeRepair.txt");
         this.puzzleRows = puzzleContents.getPuzzleRows();
     }
 
-    public void solvePuzzle1() {
-//        puzzleRows.forEach(System.out::println);
-        String calibrationEquation = puzzleRows.get(1);
+    public long solvePuzzle1() {
+        long totalCalibrationResult = 0;
+//        for (String calibrationEquation : puzzleRows) {
+//        String calibrationEquation = "26324383: 7 9 8 7 414 471 185 8";
+        String calibrationEquation = "11884578: 26 91 864 391 590 78";     // TODO Goudemond 2024/12/08 | Should this be accepted?
         String[] calibrationArguments = calibrationEquation.split(": ");
-        int result = getResult(calibrationArguments);
+        long product = getResult(calibrationArguments);
         List<String> calibrationInputs = getCalibrationInputs(calibrationArguments);
-        System.out.println("result = " + result);
-        // binary string to show all possible combos
-        List<String> allPossibleEquations = getAllPossibleEquations(result, calibrationInputs);
-        System.out.println("allPossibleEquations = " + allPossibleEquations);
-        // TODO Goudemond 2024/12/07 | implement custom evalLeftToRight()
-
+        List<String> allPossibleEquations = getAllPossibleEquations(calibrationInputs);
+        for (String expression : allPossibleEquations) {
+            long result = MathHelper.evalLeftToRight(expression);
+            System.out.printf("%s = %s%n", expression, result);
+            if (result == product) {
+                System.out.printf("%s = %s%n", expression, result);
+                totalCalibrationResult += result;
+                break;
+            }
+//            }
+        }
+        // Reddit spoilers say I should be getting something like (146111650210682)
+        // they seem to be concatenating as a 3rd operation!?!?!?!?
+        return totalCalibrationResult; // 51277701 (too low) ; 51277870 (too low)
     }
 
-    private List<String> getAllPossibleEquations(int result, List<String> calibrationInputs) {
+    private List<String> getAllPossibleEquations(List<String> calibrationInputs) {
         int numberOperatorsNeeded = calibrationInputs.size() - 1;
         int permutationCount = 0;
         String defaultExpression = String.join("+", calibrationInputs);
+        List<String> expressions = new ArrayList<>();
         List<String> operations = Arrays.stream(defaultExpression.split("(\\d+)"))
                 .collect(Collectors.toList());
-        List<String> expressions = new ArrayList<>();
         expressions.add(defaultExpression);
-        while (permutationCount <= numberOperatorsNeeded) {
+        double allPossiblePermutations = Math.pow(2, numberOperatorsNeeded) - 1;
+        while (permutationCount < allPossiblePermutations) {
             permutationCount++;
             String permutationAsBinaryString = getPermutationAsBinaryString(numberOperatorsNeeded, permutationCount);
             String expression = createExpression(calibrationInputs, permutationAsBinaryString, operations);
@@ -51,15 +64,17 @@ public class BridgeRepair {
 
     }
 
-    private static String getPermutationAsBinaryString(int numberOperatorsNeeded, int permutationCount) {
+    private String getPermutationAsBinaryString(int numberOperatorsNeeded, int permutationCount) {
         String permutationAsBinaryString = String.format("%" + numberOperatorsNeeded + "s", Integer.toBinaryString(permutationCount));
         permutationAsBinaryString = permutationAsBinaryString.replace(' ', '0');
         return permutationAsBinaryString;
     }
 
-    private static String createExpression(List<String> calibrationInputs, String formatString, List<String> operations) {
+    @SuppressWarnings("StringConcatenationInLoop")
+    private String createExpression(List<String> calibrationInputs, String formatString, List<String> operations) {
         String[] permutationAsBinaryString = formatString.split("");
         int operatorPlacementIndex = operations.size() - 1;
+        String startingInput = calibrationInputs.get(0);
         for (String permutation : permutationAsBinaryString) {
             if (permutation.equals("0")) {
                 operations.set(operatorPlacementIndex, "+");
@@ -68,27 +83,28 @@ public class BridgeRepair {
             }
             operatorPlacementIndex--;
         }
-        String startingInput = calibrationInputs.get(0);
         String expression = startingInput;
+        int j = 1;
         for (int i = calibrationInputs.size() - 1; i > 0; i--) {
-            expression += operations.get(i) + calibrationInputs.get(i);
+            expression += operations.get(i) + calibrationInputs.get(j++);
         }
         return expression;
     }
 
-    private static List<String> getCalibrationInputs(String[] calibrationArguments) {
+    private List<String> getCalibrationInputs(String[] calibrationArguments) {
         return Arrays.stream(calibrationArguments[1].split(" "))
 //                .map(Integer::parseInt)
                 .collect(Collectors.toList());
     }
 
-    private static int getResult(String[] calibrationArguments) {
-        return Integer.parseInt(calibrationArguments[0]);
+    private long getResult(String[] calibrationArguments) {
+        return Long.parseLong(calibrationArguments[0]);
     }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         BridgeRepair bridgeRepair = new BridgeRepair();
-        bridgeRepair.solvePuzzle1();
+        long puzzle1solution = bridgeRepair.solvePuzzle1();
+        System.out.println("puzzle1solution = " + puzzle1solution);
     }
 }
